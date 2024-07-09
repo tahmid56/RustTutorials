0ff2593b4ead61e40984dee5a728225be70193e0
Tahmid Akter <tahmidakter56@gmail.com>
2024-07-09 22:31:11 +0600

    Hashmap implementation

--- section1/authentication/src/lib.rs
+++ section1/authentication/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn greet_user(name: &str) -> String {
     format!("Hello {name}")
 } 
@@ -20,6 +22,7 @@ pub enum LoginAction {
     Denied,
 }
 
+#[derive(Clone, Debug)]
 pub struct User {
     username: String,
     password: String,
@@ -36,30 +39,48 @@ impl User{
     }
 }
 
-pub fn get_users() -> Vec<User> {
-    vec![
-        User::new("admin", "password", LoginRole::Admin),
-        User::new("user", "password", LoginRole::User),
-    ]
+pub fn get_users() -> HashMap<String, User>{
+    let mut users = HashMap::new();
+    users.insert("admin".to_string(), User::new("admin", "password", LoginRole::Admin));
+    return users;
 }
 
-fn get_admin_users(){
-    let users: Vec<String> = get_users()
-        .into_iter()
-        .filter(|u| u.role == LoginRole::Admin)
-        .map(|u| u.username)
-        .collect();
-}
+// pub fn get_users() -> Vec<User> {
+//     vec![
+//         User::new("admin", "password", LoginRole::Admin),
+//         User::new("user", "password", LoginRole::User),
+//     ]
+// }
+
+// fn get_admin_users(){
+//     let users: Vec<String> = get_users()
+//         .into_iter()
+//         .filter(|u| u.role == LoginRole::Admin)
+//         .map(|u| u.username)
+//         .collect();
+// }
+
 pub fn login(username: &str, password: &str) -> Option<LoginAction> {
     let username = username.to_lowercase();
     let users = get_users();
-    if let Some(user) = users.iter().find(|user| user.username == username){
+    
+    // WHILE USING HASHMAP
+    if let Some(user) = users.get(&username) {
         if user.password == password {
             return Some(LoginAction::Granted(user.role.clone()));
         }else {
             return Some(LoginAction::Denied)
         }
     }
+
+    //WHILE USING VECTOR
+    // if let Some(user) = users.iter().find(|user| user.username == username){
+    //     if user.password == password {
+    //         return Some(LoginAction::Granted(user.role.clone()));
+    //     }else {
+    //         return Some(LoginAction::Denied)
+    //     }
+    // }
     None
 }
 
