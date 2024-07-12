9da5bfcee1ec171aca5ad7e4aa378a2a2ae20eb2
Tahmid Akter <tahmidakter56@gmail.com>
2024-07-12 15:38:10 +0600

    receiving returned value from thread

--- section2/hello/src/main.rs
+++ section2/hello/src/main.rs
@@ -2,14 +2,24 @@ fn hello_thread(n: u32) {
     println!("Hello from thread {n}!");
 }
 
+fn do_math(i: u32) -> u32{
+    let mut n = i+1;
+    for _ in 0..10 {
+        n *= 2;
+    }
+    n
+}
+
 fn main() {
     println!("Hello, world!");
     let mut handle_threads = Vec::new();
-    for i in 0..5 {
-        let handle_thread = std::thread::spawn(move || hello_thread(i));
+    for i in 0..10 {
+        let handle_thread = std::thread::spawn(move || do_math(i));
         handle_threads.push(handle_thread);
     }
-    handle_threads.into_iter().for_each(|h| h.join().unwrap());
+    handle_threads.into_iter().for_each(|h| {
+        println!("{}",h.join().unwrap());
+    });
 
     // FOR SPAWNING A THREAD
     // let handle_thread = std::thread::spawn(hello_thread);
