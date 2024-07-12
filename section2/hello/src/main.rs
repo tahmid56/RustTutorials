b6af9a3a637b37e3b9617f4f2ce7012067f64528
Tahmid Akter <tahmidakter56@gmail.com>
2024-07-12 12:56:33 +0600

    Spawning threads with parameters

--- section2/hello/src/main.rs
+++ section2/hello/src/main.rs
@@ -1,9 +1,17 @@
-fn hello_thread() {
-    println!("Hello from thread!");
+fn hello_thread(n: u32) {
+    println!("Hello from thread {n}!");
 }
 
 fn main() {
     println!("Hello, world!");
-    let handle_thread = std::thread::spawn(hello_thread);
-    handle_thread.join().unwrap();
+    let mut handle_threads = Vec::new();
+    for i in 0..5 {
+        let handle_thread = std::thread::spawn(move || hello_thread(i));
+        handle_threads.push(handle_thread);
+    }
+    handle_threads.into_iter().for_each(|h| h.join().unwrap());
+
+    // FOR SPAWNING A THREAD
+    // let handle_thread = std::thread::spawn(hello_thread);
+    // handle_thread.join().unwrap();
 }
