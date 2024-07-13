9a7579e9231094fb8fd7994419af8361751a1878
Tahmid Akter <tahmidakter56@gmail.com>
2024-07-13 21:19:01 +0600

    Scoped thread added

---
+++ section2/scoped_thread/src/main.rs
@@ -0,0 +1,16 @@
+use std::thread;
+
+fn main() {
+    let N_THREADS: usize = 8;
+    let to_add: Vec<u32> = (0..5000).collect();
+    let chunks = to_add.chunks(N_THREADS);
+    let sum:u32 = thread::scope(|s| {
+        let mut thread_handles = Vec::new();
+        for chunk in chunks {
+            let thread_handle = s.spawn(move || chunk.iter().sum::<u32>());
+            thread_handles.push(thread_handle);
+        }
+        thread_handles.into_iter().map(|h| h.join().unwrap()).sum::<u32>()
+    });
+    println!("Sum: {sum}");
+}
